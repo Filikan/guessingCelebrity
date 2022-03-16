950d92c81a9ede4053e779a9beebd19de3ad8c4a
mckasap <mckasap@gmail.com>
2022-03-16 11:47:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weeek06_2022_03/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.weeek06_2022_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMe(View V){
+
+    }
+}
